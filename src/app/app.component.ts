dcc7c90e34a6059989c9489a54589b1283f1087f
nikhil_kawdeti <nikhilkawdeti28@gmail.com>
2023-05-09 16:45:02 +0530

    docs push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'covid-vaccine-system';
+
+  registrations:any = [
+    "Adams",
+    "Baker",
+    "Clark",
+    "Davis",
+    "Evans",
+    "Frank",
+    "Lopez",
+    "Mason",
+    "Quinn",
+    "Ochoa",
+    "Patel",
+  ];
+
+  firstDose:any =[];
+
+  secondDose:any =[];
+
+  boosterDose:any =[];
+
+  next(index:number){
+    this.firstDose.push(this.registrations[index]);
+    this.registrations.splice(index,1);
+  }
+  D1Done(index:number){
+    this.secondDose.push(this.firstDose[index]);
+    this.firstDose.splice(index,1);
+  }
+  D2Done(index:number){
+    this.boosterDose.push(this.secondDose[index]);
+    this.secondDose.splice(index,1);
+  }
+
+
+  delete(index:number){
+    this.registrations.splice(index,1);
+  }
+  undoToRegistrations(index:number){
+    this.registrations.push(this.firstDose[index]);
+    this.firstDose.splice(index,1);
+  }
+  undoToFirstDose(index:number){
+    this.firstDose.push(this.secondDose[index]);
+    this.secondDose.splice(index,1);
+  }
+  undoToSecondDose(index:number){
+    this.secondDose.push(this.boosterDose[index]);
+    this.boosterDose.splice(index,1);
+  }
 }
